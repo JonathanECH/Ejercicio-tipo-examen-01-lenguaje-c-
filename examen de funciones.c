bedec7830b3a2ed3f36e4b9bf00457d89fcafee1
JonathanECH <Jonathanecampos3@gamil.com>
2025-08-01 16:07:54 -0400

    Ejercicio tipo examen version original

---
+++ examen de funciones.c
@@ -0,0 +1,52 @@
+/* Para un grupo de 10 personas se tienen los siguientes datos: sexo, edad y número de hijos.
+Se desea conocer:
+a el promedio de edad de hombres (función)
+b total de hijos del grupo.
+El código es en C */
+#include <stdio.h>
+#include <string.h>
+void promedioH(int sumaEdad, int counterH)
+{
+    float promedioEdad;
+    promedioEdad = sumaEdad / counterH;
+    printf("La edad promedio es: %.2f\n", promedioEdad);
+}
+
+
+int main()
+{
+    int  edad, i, counterH = 0, sumaEdad = 0, nHijos = 0, hijos;
+    char sexo, tieneHijos;
+
+    for (i = 0; i < 10; i++)
+    {
+        printf("Cual es su sexo (M o F): ");
+        scanf(" %c", &sexo);
+        if (sexo == 'M' || sexo == 'm')
+        {
+            counterH++;
+            printf("Cual es su edad? ");
+            scanf("%d", &edad);
+            sumaEdad += edad;
+        }
+        
+
+        printf("Tiene hijos? (S o N): ");
+        scanf(" %c", &tieneHijos);
+
+        if (tieneHijos == 'S' || tieneHijos == 's')
+        {
+            printf("Cuantos hijos tiene? ");
+            scanf("%d", &hijos);
+            nHijos += hijos;
+        }
+        else if (tieneHijos == 'N' || tieneHijos == 'n')
+        {
+        }
+
+    }
+    printf("\n");
+    printf("-----Tabla de Informacion-----\n");
+    promedioH(sumaEdad, counterH);
+    printf("El total de hijos es: %d\n ", nHijos);
+}
