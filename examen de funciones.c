e8e70c5d0a44f9c0f2f9cf7b9cd1b2436857d765
JonathanECH <Jonathanecampos3@gamil.com>
2025-08-02 20:19:33 -0400

    Le agrege un metodo tolower a los if

--- examen de funciones.c
+++ examen de funciones.c
@@ -5,6 +5,7 @@ b total de hijos del grupo.
 El código es en C */
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
 void promedioH(int sumaEdad, int counterH)
 {
     float promedioEdad;
@@ -12,38 +13,36 @@ void promedioH(int sumaEdad, int counterH)
     printf("La edad promedio es: %.2f\n", promedioEdad);
 }
 
-
 int main()
 {
-    int  edad, i, counterH = 0, sumaEdad = 0, nHijos = 0, hijos;
-    char sexo, tieneHijos;
+    int edad, i, counterH = 0, sumaEdad = 0, nHijos = 0, hijos;
+    char sexo, tieneHijos, convertir;
 
-    for (i = 0; i < 10; i++)
+    for (i = 0; i < 2; i++)
     {
         printf("Cual es su sexo (M o F): ");
-        scanf(" %c", &sexo);
-        if (sexo == 'M' || sexo == 'm')
+        scanf(" %c", &convertir);
+        sexo = tolower(convertir);
+        if (sexo == 'm')
         {
             counterH++;
             printf("Cual es su edad? ");
             scanf("%d", &edad);
             sumaEdad += edad;
         }
-        
 
         printf("Tiene hijos? (S o N): ");
-        scanf(" %c", &tieneHijos);
-
-        if (tieneHijos == 'S' || tieneHijos == 's')
+        scanf(" %c", &convertir);
+        tieneHijos = tolower(convertir);
+        if (tieneHijos == 's')
         {
             printf("Cuantos hijos tiene? ");
             scanf("%d", &hijos);
             nHijos += hijos;
         }
-        else if (tieneHijos == 'N' || tieneHijos == 'n')
+        else if (tieneHijos == 'n')
         {
         }
-
     }
     printf("\n");
     printf("-----Tabla de Informacion-----\n");
